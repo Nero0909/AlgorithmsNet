b2a7f6d8ba69385e7f0e0eb2a6ba33103aaf384c
Nikita <Nero0909@yandex.ru>
2017-04-11 18:14:23 +0300

    Was added merge BU

---
+++ Algorithms.Std.Tests/Sort/MergeBUTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithms.Std.Tests.Sort
+{
+    using System;
+    using System.Collections.Generic;
+    using global::Algorithms.Std.Sort;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    internal sealed class MergeBUTests : SortBase
+    {
+        protected override Action GetSortBySelector<TSource, TKey>(IList<TSource> arr, Func<TSource, TKey> keySelector)
+        {
+            return () => MergeBU<TSource>.Sort(arr, keySelector);
+        }
+
+        protected override Action GetSortByComparer<TSource>(IList<TSource> arr, IComparer<TSource> comparer)
+        {
+            return () => MergeBU<TSource>.Sort(arr, comparer);
+        }
+
+        protected override Action GetSimpleSort<TSource>(IList<TSource> arr)
+        {
+            return () => MergeBU<TSource>.Sort(arr);
+        }
+    }
+}
---
+++ Algorithms.Std/Sort/MergeBU.cs
@@ -0,0 +1,70 @@
+﻿namespace Algorithms.Std.Sort
+{
+    using System;
+    using System.Collections.Generic;
+    using MiscUtil.Collections;
+
+    public static class MergeBU<TSource>
+    {
+        private static TSource[] tmp;
+
+        public static void Sort(IList<TSource> arr)
+        {
+            var comparer = Comparer<TSource>.Default;
+
+            Sort(arr, comparer);
+        }
+
+        public static void Sort<TKey>(IList<TSource> arr, Func<TSource, TKey> keySelector)
+        {
+            var comparer = ProjectionComparer<TSource>.Create(keySelector);
+
+            Sort(arr, comparer);
+        }
+
+        public static void Sort(IList<TSource> arr, IComparer<TSource> comparer)
+        {
+            tmp = new TSource[arr.Count];
+            var N = arr.Count;
+
+            for (var sz = 1; sz < N; sz = sz + sz)
+            {
+                for (var lo = 0; lo < N - sz; lo += sz + sz)
+                {
+                    MergeInternal(arr, comparer, lo, lo + sz - 1, Math.Min(lo + sz + sz - 1, N - 1));
+                }
+            }
+        }
+
+        private static void MergeInternal(IList<TSource> arr, IComparer<TSource> comparer, int lo, int mid, int hi)
+        {
+            var i = lo;
+            var j = mid + 1;
+
+            for (var k = lo; k <= hi; k++)
+            {
+                tmp[k] = arr[k];
+            }
+
+            for (var k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                {
+                    arr[k] = tmp[j++];
+                }
+                else if (j > hi)
+                {
+                    arr[k] = tmp[i++];
+                }
+                else if (ArrayHelper.Less(comparer, tmp[j], tmp[i]))
+                {
+                    arr[k] = tmp[j++];
+                }
+                else
+                {
+                    arr[k] = tmp[i++];
+                }
+            }
+        }
+    }
+}
